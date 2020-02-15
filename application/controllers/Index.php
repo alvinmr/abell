d6793168151f850508021810052a3274dc4893b7
alvinmr <maulanaalvin1@gmail.com>
2020-02-15 12:14:55 +0800

    nambah construct

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -4,9 +4,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Index extends CI_Controller {
 
+    
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+    
+
     public function index()
     {
-        
+                        
     }
 
 }
