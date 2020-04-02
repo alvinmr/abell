7eec85928a3a931daa929f5279519e4bea52b1b1
alvinmr <maulanaalvin1@gmail.com>
2020-04-02 22:25:01 +0800

    fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,16 @@
-// Time
-function jam() {
-	document.getElementById("jam").innerHTML =
-		"Waktu : " +
-		moment()
-			.locale("en-gb")
-			.format("LTS");
+// // Time
+// function jam() {
+// 	document.getElementById("jam").innerHTML =
+// 		"Waktu : " +
+// 		moment()
+// 			.locale("en-gb")
+// 			.format("LTS");
 
-	var jamskrng = moment()
-		.locale("en-gb")
-		.format("LTS");
-}
-setInterval(jam, 1000);
+// 	var jamskrng = moment()
+// 		.locale("en-gb")
+// 		.format("LTS");
+// }
+// setInterval(jam, 1000);
 
 // Name File Browser
 $(".custom-file-input").on("change", function() {
